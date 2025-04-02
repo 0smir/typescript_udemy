4061375715940297af594930aa72e75fe9b38b31
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-02 13:05:05 +0300

    readonly props and ways of manipulating with it

--- TS_classes/basics.ts
+++ TS_classes/basics.ts
@@ -15,7 +15,12 @@
 
 class User {
   public hobbies: string[] = [];
+  responsibilities: string[] = []; // will be public, because it is default. Privat should be assigned explisitly
+
+  readonly possitions: string[] = [];
+
   private userType: string = 'user';
+
   constructor(public name: string, public age: number) { };//alernative method to add properties
   greet() {
     console.log('userType', this.userType)
@@ -27,5 +32,6 @@ const hel = new User('Helga', 33);
 const max = new User('Max', 96);
 
 max.age = 37; // publick key-word could be reassigned
-
+// max.possitions = ['mentor']; //incorrect way - you try to set new array due this prop-ty is readonly
+max.possitions.push('barber'); // correct way  - you set new value to existing array (manipulation with original array)
 console.log(hel, max);
