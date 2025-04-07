cbd7b4d7debf8487022174f43db79c02e4fd9088
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-07 19:38:57 +0300

    creating and usage of Generic pattern

---
+++ generic_type/1_generic.ts
@@ -0,0 +1,20 @@
+//============== Generic Types ======
+// It describes two  kind of types working together - Array<string> it mean 'array' full of 'strings'
+//So Generic is combinations of types - it give possibility to build more flexible things.
+
+// let names: string[] = ['Max', 'Helga'];//
+let names: Array<string> = ['Max', 'Helga']; // it is same as prev line
+//here we using existing Array type (interface Array<T>). 
+
+
+//================ Creating and Usage Generic type ====================
+
+type DataStorage<T> = { // we create a custom Generic type, where T is for type and can be any other character (K, anything, etc)
+  [key: string]: T
+}
+
+let store: DataStorage<string | boolean> = {};
+store.name = 'Helga';
+store.isInstruct = false;
+
+let namestore: DataStorage<string> = {};
