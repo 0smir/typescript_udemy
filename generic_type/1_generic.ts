2e144972263e922b6fc0c11216477b7752692171
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-07 20:13:46 +0300

    generic with constrains and multiple params of generic functions (with constrains or not)

--- generic_type/1_generic.ts
+++ generic_type/1_generic.ts
@@ -32,3 +32,19 @@ function mergeAll<T, U>(a: T, b: U) { // you can assign types as may as you need
 }
 
 const arrayResult = mergeAll(1, 'Helga');
+
+
+//=============  Generic && Constraints (restrictions)================
+
+function mergeObj<T extends object>(a: T, b: T) { //you can add some restrictions to Generic type parameters, because not all values can be assigned
+  // in this case we expect objects and restrict T (type) as object
+  return { ...a, ...b }
+}
+
+let merged = mergeObj({ name: 'Max' }, { name: 'Helga' });// correct
+let mergedObj = mergeObj({ name: 'Max' }, { age: 32 });//correct, even if age value type is 'number' due T is ution type (string|number{})
+
+function mergObjUT<T extends object, U extends object>(a: T, b: U) {// same as prev example
+  // bouth params extends object
+  return { ...a, ...b }
+}
