b92f557ff6d536ffd8692399d93a4a20f8fa5a2d
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-11 19:58:24 +0300

    resolve task to add item on  positionX, remove iten on position X

--- linked_list_demo/linked_list.ts
+++ linked_list_demo/linked_list.ts
@@ -5,13 +5,18 @@ class ListNode<T> {
 
 class LinkedList<T> {
   private tail?: ListNode<T>;
-  constructor(private root?: ListNode<T>, private length = 0) { }
+  constructor(
+    private root?: ListNode<T>,
+    private length = 0
+  ) { }
+
   add(value: T) {
     const node = new ListNode(value);
     if (!this.root || !this.tail) {
       this.root = node;
       this.tail = node;
     } else {
+
       // let current = this.root;//long approach
       // while (current.next) { 
       //   current = current.next;
@@ -34,10 +39,51 @@ class LinkedList<T> {
     }
   }
   insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let prev = current;
+      let index = 0;
+      let node = new ListNode(value);
 
+      if (pos === 0) {
+        node.next = this.root;
+        this.root = node;
+      } else {
+        while (index++ < pos && current.next) {
+          console.log('while: ', index, pos);
+          prev = current;
+          current = current.next;
+        }
+        node.next = current;
+        prev.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  removeAt(pos: number) { }
+  removeAt(pos: number) {
+    if (pos >= 0 && this.length > 0 && this.root) {
+      let current = this.root,
+        index = 0,
+        prev: ListNode<T> = current;
+      if (pos === 0) {
+        this.root = current.next;
+      } else {
+        while (index++ < pos && current.next) {
+          prev = current;
+          current = current.next;
+        }
+        prev.next = current.next;
+      }
+      this.length--;
+      return current;
+    } else {
+      return null;
+    }
+  }
 }
 
 
@@ -48,10 +94,17 @@ numberList.add(2);
 numberList.add(3);
 numberList.add(5);
 numberList.add(-6);
-
-
 console.log('length: ', numberList.getNumberOfElements());
+
+numberList.insertAt(100, 0);
+numberList.insertAt(50, 3);
 numberList.print();
 
+console.log('=================================');
+
+numberList.removeAt(0);
+numberList.removeAt(2);
+// numberList.removeAt(numberList.getNumberOfElements() - 1);
+numberList.print();
 
 const nameList = new LinkedList<string>();
