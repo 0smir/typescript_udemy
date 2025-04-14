1f9263f48465ad7b91bb82a6739001c6c850bfa4
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-14 22:28:39 +0300

    add notes for infers usage

--- TS_typeof/7_infer.ts
+++ TS_typeof/7_infer.ts
@@ -5,5 +5,6 @@ function add(a: number, b: number) {
 type AddFn = typeof add;
 type ReturnValueType<T> = T extends (...args: any[]) => infer R ? R : T;
 
-
 type AddFnReturnValueType = ReturnValueType<AddFn>;
+// type AddFnReturnValueType = ReturnType<AddFn>;// can be used in this way due ReturnType is build in TS feature utilit
+//you can check all : https://www.typescriptlang.org/docs/handbook/utility-types.html
