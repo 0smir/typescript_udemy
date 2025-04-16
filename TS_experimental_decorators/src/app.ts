657fd0fd0f6bdba3f9e0a7f66a5e165730a7d2d6
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-04-16 15:50:50 +0300

    multiple decorators and order of executing of decorator Factories and decorator functions

--- TS_experimental_decorators/src/app.ts
+++ TS_experimental_decorators/src/app.ts
@@ -1,5 +1,7 @@
 
 function Logger(logString: string) {
+  console.log('Logger Factory');
+
   return function (constructor: Function) { // in this case 'constructor' is a name of argument, you can use any other name: 'target', 'targ', 'cnstr' etc.
     console.log('Logging...', logString);
     console.log('constructor: ', constructor);
@@ -7,8 +9,11 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('template Factory');
   // return function (_: Function) {//(_) replace 'constructor', due I know we get this argument, but I don't need it.
   return function (constructor: any) {//(_) replace 'constructor', due I know we get this argument, but I don't need it.
+    console.log('Rendering template');
+
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -20,9 +25,8 @@ function WithTemplate(template: string, hookId: string) {
 
 
 // decorator will runs when it finds your class definition (constructor function definition)
-//@Logger('LOGGING - PERSON') // @-character is a sign of decorator calling
-
-@WithTemplate('<h1>My Person object</h1>', 'app')
+@Logger('LOGGING - PERSON') // @-character is a sign of decorator calling
+@WithTemplate('<h1>My Person object</h1>', 'app') //order of executing of decoretor (actual decorator functions) is bottom up (bottom runs first), but decoretor Factories run earlier
 class Person {
   name = "Helga";
   constructor() {
