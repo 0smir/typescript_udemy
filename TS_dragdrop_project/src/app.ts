36b41f67e782563c6c463336f72e1f238f9ffef7
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-05-06 15:17:43 +0300

    add main logic sructure for test-app, autobind decorator, add form submit-logic

--- TS_dragdrop_project/src/app.ts
+++ TS_dragdrop_project/src/app.ts
@@ -1,9 +1,31 @@
 // Code goes here!
+//autobind decorator
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor) {
 
+  const originMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+
+
+//Project Input Class
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   formEl: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
 
   constructor() {
     this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -11,12 +33,57 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateEl.content, true);
     this.formEl = importedNode.firstElementChild as HTMLFormElement;
+    this.formEl.id = 'user-input';
 
+    this.titleInputEl = this.formEl.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputEl = this.formEl.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputEl = this.formEl.querySelector('#people')! as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputEl.value;
+    const enteredDescription = this.descriptionInputEl.value;
+    const enteredPeople = this.peopleInputEl.value;
+
+    if (enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0) {
+      alert('Invalid values, please check');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+  private clearInputs() {
+    this.titleInputEl.value = '';
+    this.descriptionInputEl.value = '';
+    this.peopleInputEl.value = '';
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) { // check is userInput is tuple (same as Array) 
+      const [title, description, people] = userInput;
+      console.log("title, description, people: ", title, description, people);
+      this.clearInputs();
+    }
+
+  }
+
+  private configure() {
+    this.formEl.addEventListener('submit', this.submitHandler);
+    // this.formEl.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
   private attach() {
     this.hostEl.insertAdjacentElement('afterbegin', this.formEl);
   }
 }
 
 const projInput = new ProjectInput();
+
