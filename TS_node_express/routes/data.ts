98b86b66cd82db181f2b9c1733f284e49c8b296f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-05-22 13:53:21 +0300

    managing data with typescript (add functions to get set and updata data of todo-list)

---
+++ TS_node_express/routes/data.ts
@@ -0,0 +1,35 @@
+interface Todo {
+  id: number,
+  text: string
+}
+
+let TODOS: Todo[] = [];
+
+export function addTodo(text: string) {
+  const newTodo = { id: Math.random(), text };
+  TODOS.push(newTodo);
+  return newTodo;
+}
+
+
+export function getTodo(id: number) {
+  const todo = TODOS.find(td => td.id === id);
+  if (!todo) {
+    throw new Error('Todo not found!');
+  }
+  return todo;
+}
+
+export function getTodos() {
+  return TODOS;
+}
+
+export function removeTodo(id: number) {
+  TODOS = TODOS.filter(td => td.id !== id);
+}
+
+export function updateTodo(id: number, text: string) {
+  let todo = getTodo(id);
+  todo.text = text;
+  return todo;
+}
